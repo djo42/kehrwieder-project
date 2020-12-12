cf6094d5a7f6f8cabf6d93550cf5710791dfc194
Dennis John <dennis.john11@gmail.com>
2020-12-13 00:57:15 +0100

    Add jsonpath

--- sixt-res/src/App.js
+++ sixt-res/src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg'
+import './App.css'
+import { getVehicles } from './services'
 
 function App() {
+  let abfrage
+  let piclink = ''
+  /*   ;(
+
+    return display(abfrage)
+  })()
+
+  function display(result) {
+
+
+    const link = result[0]['ImageUrl']
+
+    console.log(link) */
+
+  //console.log(link)
+
+  //const link =
+
+  ;(async () => {
+    abfrage = await getVehicles()
+    
+    piclink = abfrage.Result.Cars.ImageUrl
+
+  })()
+
+  console.log(JSON.stringify(abfrage))
+  console.log(piclink)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={piclink} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +48,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- sixt-res/src/services.js
+++ sixt-res/src/services.js
@@ -1,6 +1,9 @@
 //API access via backend
 
+import axios from 'axios'
+
 export async function getVehicles() {
-    const res = await fetch('http://localhost:4000/api/sixt/vehmodellist?cit=11')
-    return res.json()
+    const res = await axios.get('http://localhost:4000/api/sixt/vehmodellist?cit=11')
+    console.log('Services.js: ' + JSON.stringify(res))
+    return res
 }
