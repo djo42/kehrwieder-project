0f26e65624755351c3777f06b1bb7b8795e5c618
Dennis John <dennis.john11@gmail.com>
2020-12-14 00:38:02 +0100

    Add first component to show available cars

--- sixt-res/src/App.js
+++ sixt-res/src/App.js
@@ -2,35 +2,70 @@
 import './App.css'
 import { getVehicles } from './services'
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
-function App() {
+export default function App() {
   const [list, setList] = useState([])
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     getVehicles().then((res) => setList(res.data.Result.Cars))
-    //.then((datalist) => setList(datalist.Result))
-  }, [])
-
-  const link = (list[0].ImageUrl)
+  }, [count])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={link} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {list.map((car, index) => (
+        <Card key={index} {...car}>
+          <img className="Car-Pic" src={list[index].ImageUrl} alt="cool car" />
+          <Button>Rent me</Button>
+        </Card>
+      ))}
+    </>
   )
 }
 
-export default App
+const Card = styled.div`
+  background: white;
+  border-radius: 3px;
+  border: lightgrey;
+  border-size: 1px;
+  box-shadow: 4px 4px 5px 2px rgba(0, 0, 255, .2);
+  color: white;
+  margin: 2em 1em;
+  padding: 1em 1em;
+
+  }
+`
+const Button = styled.button`
+  display: flexbox;
+  margin-left: auto;
+  margin-right: auto;
+  border: none;
+  padding: 1rem 2rem;
+  margin: 0;
+  text-decoration: none;
+  background: forestgreen;
+  color: white;
+  font-family: sans-serif;
+  font-size: 1rem;
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, 
+              transform 150ms ease;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  }
+
+  &:hover,
+  &:focus {
+  background: #0053ba;
+  }
+
+  &:focus {
+  outline: 1px solid #fff;
+  outline-offset: -4px;
+  }
+
+  &:active {
+  transform: scale(0.99);
+}`
