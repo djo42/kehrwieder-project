a85a4c13ae6aaba67183090251283d8f2a308443
Dennis John <dennis.john11@gmail.com>
2020-12-28 00:03:08 +0100

    Update Request Form

--- sixt-res/src/Offerform.js
+++ sixt-res/src/Offerform.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import './App.css'
+import {
+  Formcontainer,
+  Formheadline,
+  Inputcontainer,
+  BranchInput,
+  Input,
+  Label,
+} from './Forms'
 
 export default function Offerdataform({ handleClick }) {
   const initialFormData = Object.freeze({
@@ -57,136 +64,45 @@ export default function Offerdataform({ handleClick }) {
   }
 
   return (
-    <Offerform>
+    <Formcontainer>
+      <Formheadline>Please enter your request details</Formheadline>
+      <Inputcontainer>
+        <Input type="checkbox" id="agencypp" name="agencypp" />
+        <Label htmlFor="agencypp">Agency Prepaid</Label>
+      </Inputcontainer>
+      <Inputcontainer>
+        <Label htmlFor="cocity">Pick-up branch</Label>
+        <BranchInput id="cocity" name="uci" type="text" onChange={handleChange} />
+      </Inputcontainer>
+      <Inputcontainer>
+        <Label htmlFor="cicity">Return branch</Label>
+        <BranchInput id="cicity" name="rci" type="text" onChange={handleChange} />
+      </Inputcontainer>
 
-   <Offerinput type="checkbox" id="agencypp" name="agencypp" /><Offerformlabel htmlFor="agencypp">Agency Prepaid</Offerformlabel><Zeilenwechsel />
+      
+        <Inputcontainer>
+          <Inputcontainer>
+            <Label htmlFor="codat">Pick-up date</Label>
+            <Input id="codat" name="uda" type="date" onChange={handleChange} />
+          </Inputcontainer>
+          <Inputcontainer>
+            <Label htmlFor="cotime">Pick-up time</Label>
+            <Input id="cotime" name="uti" type="time" onChange={handleChange} />
+          </Inputcontainer>
+        </Inputcontainer>
+        <Inputcontainer>
+          <Inputcontainer>
+            <Label htmlFor="cidat">Return date</Label>
+            <Input id="cidat" name="rda" type="date" onChange={handleChange} />
+          </Inputcontainer>
+          <Inputcontainer>
+            <Label htmlFor="citime">Return time</Label>
+            <Input id="citime" name="rti" type="time" onChange={handleChange} />
+          </Inputcontainer>
+        </Inputcontainer>
   
 
-      <Offerformlabel htmlFor="cocity">
-        <Offerinput
-          id="cocity"
-          name="uci"
-          type="text"
-          onChange={handleChange}
-        />
-        Pick-up branch
-      </Offerformlabel>
-
-      <Offerformlabel htmlFor="codat">
-        <Offerinput id="codat" name="uda" type="date" onChange={handleChange} />
-        Pick-up date
-      </Offerformlabel>
-      <Offerformlabel htmlFor="cotime">
-        <Offerinput
-          id="cotime"
-          name="uti"
-          type="time"
-          onChange={handleChange}
-        />
-        Pick-up time
-      </Offerformlabel>
-
-      <Offerformlabel htmlFor="cicity">
-        <Offerinput
-          id="cicity"
-          name="rci"
-          type="text"
-          onChange={handleChange}
-        />
-        Return branch
-      </Offerformlabel>
-
-      <Offerformlabel htmlFor="cidat">
-        <Offerinput id="cidat" name="rda" type="date" onChange={handleChange} />
-        Return date
-      </Offerformlabel>
-
-      <Offerformlabel htmlFor="citime">
-        <Offerinput
-          id="citime"
-          name="rti"
-          type="time"
-          onChange={handleChange}
-        />
-        Return time
-      </Offerformlabel>
-
       <button onClick={handleSubmit}>Send request</button>
-    </Offerform>
+    </Formcontainer>
   )
 }
-
-const Offerform = styled.form`
-  box-sizing: border-box;
-  width: 100%;
-  font-size: 1em;
-  font-family: Roboto;
-  border: 1px solid;
-  background: white;
-  border-radius: 3px;
-  border: lightgrey;
-  border-size: 1px;
-  box-shadow: 4px 4px 5px 2px rgba(0, 0, 255, 0.2);
-  color: white;
-  margin: 20px;
-  padding: 1em 1em;
-  label, input, select, textarea {
-    display: block;
-    float: left;
-`
-
-
-const Offerinput = styled.input`
-    display: block;
-    float: left;
-    font-size: 14px;
-    font-family: Roboto;
-    display: block;
-    color: purple;
-    content: "Hello";
-    top: 14px;
-    right: 10px;
-    width: 33%;
-    height: 1.5rem;
-    margin: 10px;
-    border-bottom: 2px grey !important;
-    }
-    `
-
-const Offerformlabel = styled.label`
-  display: block;
-  float: left;
-  text-align:left;
-  font-size: 1em;
-  color: purple;
-  margin: 5px;
-`
-
-const Zeilenwechsel = styled.br`
-  clear: left;
-`
-
-/* <form>
-<label for="name">Name</label><input type="text" name="name" /><br />
-<label for="email">E-Mail</label><input type="text" name="email" /><br />
-<label for="betreff">Betreff:</label><input type="text" name="betreff" /><br />
-</form> */
-
-
-/* form{
-// width: xxxpx;
-}
-label, input, select, textarea {
-display: block;
-float: left;
-}
-label {
-// width: xxxpx;
-// text-align: right;
-}
-input, select, textarea {
-// width: xxxpx;
-}
-form br {
-clear: left;
-} */
